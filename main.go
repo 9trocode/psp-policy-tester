ba8207a47d59161fbcfe97d4c32d821aaa9875dc
Replit user <>
2023-02-09 18:56:39 +0000

    update

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"reflect"
-  
+	// "reflect"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -15,7 +15,7 @@ func main() {
 	// Create a new Kubernetes client using ClusterInConfig
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		fmt.Println("Error creating config:", err)
+		fmt.Println("Error Connecting to config:", err)
 		return
 	}
 	clientset, err := kubernetes.NewForConfig(config)
@@ -31,11 +31,11 @@ func main() {
 		return
 	}
 
-  fmt.Println(psps)
+	fmt.Println(psps)
 
-    privileged := true
-		hostPID := true
-		hostNetwork := true
+	privileged := true
+	hostPID := true
+	hostNetwork := true
 
 	// Attempt to create a pod that violates the PSP rules
 	pod := &corev1.Pod{
@@ -43,37 +43,38 @@ func main() {
 			Name: "test-pod",
 		},
 		Spec: corev1.PodSpec{
-				Containers: []corev1.Container{
-					{
-						Name:  "test-container",
-						Image: "test-image",
-						SecurityContext: &corev1.SecurityContext{
-							// Attempt to violate the PSP rules by running as privileged
-							Privileged: &privileged,
-						},
+			Containers: []corev1.Container{
+				{
+					Name:  "test-container",
+					Image: "test-image",
+					SecurityContext: &corev1.SecurityContext{
+						// Attempt to violate the PSP rules by running as privileged
+						Privileged: &privileged,
 					},
 				},
-				// Attempt to violate the PSP rules by running as a host PID
-				HostPID: hostPID,
-				// Attempt to violate the PSP rules by using a host network
-				HostNetwork: hostNetwork,
 			},
+			// Attempt to violate the PSP rules by running as a host PID
+			HostPID: hostPID,
+			// Attempt to violate the PSP rules by using a host network
+			HostNetwork: hostNetwork,
+		},
 	}
 
 	psp, err := clientset.CoreV1().Pods("*").Create(context.TODO(), pod, metav1.CreateOptions{})
 	if err != nil {
+		fmt.Println(psp)
 		// Check if the error is due to PSP violation
-		status, ok := err.(*metav1.Status)
-		if ok && status.Code == 403 && status.Reason == "Forbidden" &&
-			reflect.DeepEqual(status.Details.Kind, "podsecuritypolicies") {
-			fmt.Println("PSP rule", psp.Name, "is enforced.")
+		// status, ok := err.
+		// if ok && status.Code == 403 && status.Reason == "Forbidden" &&
+		// 	reflect.DeepEqual(status.Details.Kind, "podsecuritypolicies") {
+		// 	fmt.Println("PSP rule", psp.Name, "is enforced.")
 
-		} else {
-			// If no error, then the PSP rule was not enforced
-			fmt.Println("PSP rule", psp.Name, "is not enforced.")
-			// Delete the pod to clean up
-			clientset.CoreV1().Pods("default").Delete(pod.Name, &metav1.DeleteOptions{})
-		}
+		// } else {
+		// 	// If no error, then the PSP rule was not enforced
+		// 	fmt.Println("PSP rule", psp.Name, "is not enforced.")
+		// 	// Delete the pod to clean up
+		// 	clientset.CoreV1().Pods("*").Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
+		// }
 	}
 
 	// Report on the current security level for multi-tenancy

55c5ce6609aeea5d772077ebdda15acb72dba394
Replit user <>
2023-02-09 18:38:07 +0000

    update

--- main.go
+++ main.go
@@ -32,37 +32,37 @@ func main() {
 		return
 	}
 
-		// Attempt to create a pod that violates the PSP rules
-		pod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "test-pod",
-			},
-			Spec: corev1.PodSpec{
-				Containers: []corev1.Container{
-					{
-						Name:  "test-container",
-						Image: "test-image",
-						SecurityContext: &corev1.SecurityContext{
-							// Attempt to violate the PSP rules by running as privileged
-							Privileged: &[]bool{true}[0],
-						},
+	// Attempt to create a pod that violates the PSP rules
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-pod",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "test-container",
+					Image: "test-image",
+					SecurityContext: &corev1.SecurityContext{
+						// Attempt to violate the PSP rules by running as privileged
+						Privileged: &[]bool{true}[0],
 					},
 				},
-				// Attempt to violate the PSP rules by running as a host PID
-				HostPID: &[]bool{true}[0],
-				// Attempt to violate the PSP rules by using a host network
-				HostNetwork: &[]bool{true}[0],
 			},
-		}
+			// Attempt to violate the PSP rules by running as a host PID
+			HostPID: &[]bool{true}[0],
+			// Attempt to violate the PSP rules by using a host network
+			HostNetwork: &[]bool{true}[0],
+		},
+	}
 
-		_, err := clientset.CoreV1().Pods("default").Create(pod)
-		if err != nil {
-			// Check if the error is due to PSP violation
-			status, ok := err.(*metav1.Status)
-			if ok && status.Code == 403 && status.Reason == "Forbidden" &&
-				reflect.DeepEqual(status.Details.Kind, "podsecuritypolicies") {
-				fmt.Println("PSP rule", psp.Name, "is enforced.")
-		
+	_, err := clientset.CoreV1().Pods("default").Create(pod)
+	if err != nil {
+		// Check if the error is due to PSP violation
+		status, ok := err.(*metav1.Status)
+		if ok && status.Code == 403 && status.Reason == "Forbidden" &&
+			reflect.DeepEqual(status.Details.Kind, "podsecuritypolicies") {
+			fmt.Println("PSP rule", psp.Name, "is enforced.")
+
 		} else {
 			// If no error, then the PSP rule was not enforced
 			fmt.Println("PSP rule", psp.Name, "is not enforced.")
@@ -74,4 +74,4 @@ func main() {
 	// Report on the current security level for multi-tenancy
 	fmt.Println("Current security level for multi-tenancy: UNKNOWN")
 	fmt.Println("More analysis and testing is required to determine the exact security level.")
-
+}
